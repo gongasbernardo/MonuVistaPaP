a0335a52b0de5659ea970fe35838c1b7fa07cd7c
gongasbernardo <goncalobernardo2008@gmail.com>
2026-05-14 09:15:07 +0000

    backend seperate

--- backend/src/routes/stats.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserStats } = require('../controllers/statsController');
-const { protect } = require('../middleware/auth');
-
-router.get('/', protect, getUserStats);
-
-module.exports = router;
--- backend/src/routes/vision.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { recognizeMonument } = require('../controllers/visionController');
-
-const router = express.Router();
-
-router.post('/recognize', recognizeMonument);
-
-module.exports = router;
